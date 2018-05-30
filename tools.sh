6e20d8a6c13e3ff1e66c1e69095f60b03aef932e
thehackingisreal <37954157+thehackingisreal@users.noreply.github.com>
2018-05-30 15:49:11 +0000

    Add files via upload

---
+++ tools.sh
@@ -0,0 +1,87 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "deface"
+sleep 1
+echo "\033[31;1m Macam-macam tool "
+sleep 1
+echo "\033[33;1m Author: the dark night -/"
+sleep 1
+echo "\033[32;1m ★contack: +6288214569363"
+sleep 1
+echo "\033[34;1m ★Indonesia Security Lite"
+sleep 1
+echo "\033[36;1m tools Yang Tersedia :"
+echo "1. webdav" 
+echo "2. reversi ip"
+echo "3. comfabrik"
+echo "4. spammer"
+echo "5. 6hash tools"
+echo "0. Keluar"
+echo "\033[30;1m99. Informasi Tool Ini"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[34;1m"
+python2 webdav.py
+fi
+if
+[ $mrrm = 99 ] || [ $mrrm = 99 ]
+then
+clear
+toilet -f slant --gay "LiteSpam"
+echo "\033[31;1mNama tools: pergabungan antara tools lain"
+sleep 1
+echo "\033[33;1mKarya: the dark night"
+sleep 1
+echo "\033[32;1mVersi: v2"
+sleep 1
+echo "\033[36;1mTeam: ★Indonesia Security Lite★"
+sleep 1
+echo "★tools ini pergabungan dri tools deface★"
+sleep 3
+echo "semoga berhasil "
+echo "\033[30;1m tunggu 3 detik "
+sleep 3
+sh coba.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m Sampai berjumpa lagi :)"
+sleep 1
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "revip"
+python2 revip.py
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+echo "\033[31;1m"
+python2 comfabrik.py
+fi
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f slant --gay "LiteSpam"
+sh LITESPAM.sh
+fi
+
+if [ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+toilet -f slant --gay "hasher"
+python2 hash.py
+fi
